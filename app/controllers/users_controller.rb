0103fde021ac1a70afc15816d06b70b7e8c9eafe
Kilian Zimmermann <kilian.zimmermann@kellogg.northwestern.edu>
2024-05-30 19:29:07 +0000

    30.05

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
 
 def users
-  
+end
+
+def index
+  @places = Place.all
 end
 
   def new
@@ -12,7 +15,13 @@ end
     @user["username"] = params["username"]
     @user["email"] = params["email"]
     @user["password"] = BCrypt::Password.create(params["password"])
+
+    if @user.username.present? && @user.email.present? && @user.password.present?
     @user.save
     redirect_to "/"
+    else
+      flash["notice"] = "Please fill in all fields."
+      render :new
+    end
   end
 end
